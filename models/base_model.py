129431391088669405cf572217e017242063ea57
2Kelvin <rocketmancodes02@gmail.com>
2023-09-20 16:48:07 -0700

    code fix

--- models/base_model.py
+++ models/base_model.py
@@ -25,25 +25,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if kwargs:
-            for k, vl in kwargs.items():
-                if k == 'created_at' or k == 'updated_at':
-                    t = datetime.strptime(t, '%Y-%m-%dT%H:%M:%S.%f')
-                if k != '__class__':
-                    setattr(self, k, vl)
-                if 'id' not in kwargs:
-                    self.id = str(uuid.uuid4())
-                if 'created_at' not in kwargs:
-                    self.created_at = datetime.now()
-                if 'created_at' in kwargs and 'updated_at' not in kwargs:
-                    self.updated_at = self.created_at
-                else:
-                    self.created_at = datetime.now()
-                    self.updated_at = datetime.now()
-        else:
+        if not kwargs:
+            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
